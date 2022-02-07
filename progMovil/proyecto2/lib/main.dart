7f19cbb32fac5abe0b0a7e29abaa89a45ba2e28f
RUS0 <alejandrofan2@gmail.com>
2022-02-07 10:11:36 +0100

    exercises

---
+++ progMovil/proyecto2/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto2/src/pages/alert_page.dart';
+import 'package:proyecto2/src/pages/card_page.dart';
+import 'package:proyecto2/src/pages/home_page.dart';
+import 'package:proyecto2/src/pages/input_page.dart';
+import 'package:proyecto2/src/pages/slider_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proyecto 2',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Conponentes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return MaterialApp(
+      title: 'Proyecto 2',
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/card': (context) => CardPage(),
+        '/alert': (context) => const AlertPage(),
+        '/slider': (context) => const SlidePage(),
+        '/inputs': (context) => InputPage(),
+      },
+    );
+  }
+}
